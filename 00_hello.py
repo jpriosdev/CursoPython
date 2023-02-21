f3e8b3ef185deae8f5f6c0ed49400059562d6342
Juan Pablo Rios <ultrademond@gmail.com>
2023-02-21 11:10:28 -0500

    Primera hora de clase

---
+++ 00_hello.py
@@ -0,0 +1,20 @@
+# esto es un comentario:
+# Nuestro hola mundo en python
+print("Hola Python")
+
+
+my_string_var = "My string variable"
+my_int_var = 5
+my_bool_var = True
+
+print(type("Hola Python")) # type me dice el tipo de la variable
+print(str(my_int_var)) # str convierte una cadena en string
+print(type(my_int_var)) #type muestra el tipo de Variable
+print("Ejemplo de mensaje",my_string_var, my_int_var, my_bool_var) # concatenación de variables y textos
+print(len(my_string_var))  # tamaño del contenido de la variable
+name, surname, alias, edad = "Juan", "Rios", "ultrademond", 41 #variables en una sola linea
+
+
+
+
+
